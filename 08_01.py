3ddc39050e682cbe2a6cadf3a78a3b0af5e6303f
nbovee <12849851+nbovee@users.noreply.github.com>
2022-12-15 00:06:48 -0500

    day 8 light cleanup

--- 08_01.py
+++ 08_01.py
@@ -3,7 +3,7 @@ from math import prod
 forest = advent.get_input(__file__)
 forest = [list(map(int,line)) for line in forest]
 
-def get_row_halves(index, data): # leftmost is always the tree for comparison. simple max would fail by looking past tree
+def get_row_halves(index, data): # leftmost is always the tree for comparison
     row, col = index
     return data[row][col::-1], data[row][col:]
 
@@ -19,23 +19,20 @@ def is_visible(tree_input):
     
 def view_range(tree_input):
     tree, other_trees = tree_input[0], tree_input[1:]
-    other_trees = list(map(lambda n: 1 if n>= tree else 0, other_trees))
+    other_trees = list(map(lambda n: 1 if n>= tree else 0, other_trees)) # filter for trees >= tree
     return other_trees.index(1) + 1 if 1 in other_trees else len(other_trees) # view to tree + index offset, or full view to edge
 
 def get_views(index):
     return *get_row_halves(index, forest), *get_col_halves(index, forest)
 
 visible_count = 0
-for i in range(len(forest)): # this could be double comprehension but we sleepy
+highest_scenic_score = 0
+
+for i in range(len(forest)):
     for j in range(len(forest[0])):
         visible_count += 1 if len(list(filter(is_visible,get_views((i,j))))) > 0 else 0
-
-print(f"{visible_count} visible trees")
-
-highest_scenic_score = 0
-for i in range(len(forest)): # this could be double comprehension but we sleepy
-    for j in range(len(forest[0])):
         candidate = prod(map(view_range,get_views((i,j))))
         highest_scenic_score = candidate if candidate > highest_scenic_score else highest_scenic_score
 
+print(f"{visible_count} visible trees")
 print(f"most scenic tree: {highest_scenic_score}")
