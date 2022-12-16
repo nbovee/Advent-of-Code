e7036774c27a63f8cedd5b9d6d5b5c42cbd359a8
nbovee <12849851+nbovee@users.noreply.github.com>
2022-12-16 01:06:36 -0500

    day 9

--- 09_01.py
+++ 09_01.py
@@ -3,85 +3,94 @@ instructions = advent.get_input(__file__)
 
 class Rope():
     def __init__(self, len) -> None:
-        self.knots = [Knot() for x in range(len)]
+        self.knots = [RopeSegment() for x in range(len)]
         
-    def tail_visited(self, index):
-        return self.knots[index].tail_visited
+    tail_visited = lambda self, index: self.knots[index].tail_visited
+
+    __str__ = lambda self: self.display_tail(len(self.knots)-1)
     
     def move(self, direction, count):
         for i in range(int(count)):
-            self._move_one(direction)
+            self.__move_one(direction)
 
-    def _move_one(self, direction):
+    def __move_one(self, direction):
         self.knots[0]._move_one(direction)
         for i in range(1, len(self.knots)):
-            # set head to tail of previous, tail will catch up
-            self.knots[i]._set_head_loc(self.knots[i-1].tail_loc)
+            self.knots[i]._set_head_loc(self.knots[i-1].tail_loc) # set head to tail of previous, tail will catch up
+    
+    def __update_display_bounds(self):
+        self.display_bounds = [
+            min(list(zip(*rope.tail_visited(0)))[0])-1,   # -x
+            max(list(zip(*rope.tail_visited(0)))[0])+2,   # +x
+            min(list(zip(*rope.tail_visited(0)))[1])-2,   # -y
+            max(list(zip(*rope.tail_visited(0)))[1])+2    # +y
+        ]
 
+    def display_tail(self, index):
+        self.__update_display_bounds()
+        s = self.knots[index].tail_visited
+        output = ''
+        for col in range(self.display_bounds[2], self.display_bounds[3]):
+            for row in range(self.display_bounds[0], self.display_bounds[1]):
+                nchar = '#' if (row, col) in s else '.'
+                for i in range(len(self.knots)-1,-1,-1): # reverse list for display priority
+                    nchar = str(i+1) if self.knots[i].tail_loc == (row, col) else nchar
+                nchar = 'H' if (row, col) == self.knots[0].head_loc else nchar
+                nchar ='s' if (row, col) == (0,0) else nchar
+                output += nchar
+            output += '\n'
+        return output.strip()
 
-class Knot():
-    def __init__(self) -> None:
-        self.max_dist = 1
-        self.head_loc = (0,0)
-        self.tail_loc = (0,0)
+class RopeSegment():
+    def __init__(self, child = None) -> None: # child here for if we revisit to remove redundant head values in the rope
+        self.head_loc, self.tail_loc = (0,0), (0,0)
         self.tail_visited = set()
         self.tail_visited.add(self.tail_loc)
-
-        self.direction_dict = {
-            'R' : (0,1),
-            'U' : (1,0),
-            'L' : (0,-1),
-            'D' : (-1,0)
+        self.direction_dict = { # offsets in the display field rather than what humans expect row and column to be 
+            'R' : (1,0),    # move right 1 in row (move over col)
+            'U' : (0,-1),   # move into an earlier row (move in same col)
+            'L' : (-1,0),   # move left 1 in row (move over col)
+            'D' : (0,1)     # move 1 into a later row (move in same col)
         }
-
-    def __str__(self) -> str:
-        return f"H:{self.head_loc} T:{self.tail_loc}"
         
-    __repr__ = __str__
-        
-    def add_coordinates(self, a, b):
+    def add_coordinates(self, a, b): # tuple operation elementwise. I will not import numpy for advent unless forced to
         assert len(a) == len(b) == 2
-        return tuple(i + j for i,j in zip(a,b))
+        return (a[0] + b[0], a[1] + b[1]) #simple way for debugging
 
-    def get_distance(self, a, b):
-        return self.add_coordinates(a, tuple(-1*e for e in b))
-    
-    def _set_head_loc(self, new_location): # distances of new not checked, be careful
-        self.update_tail(new_location, self.head_loc)
-        self.head_loc = new_location 
-
-    def move(self, direction, count):
-        for i in range(int(count)):
-            self._move_one(direction)
+    def sub_coordinates(self, a, b):
+        assert len(a) == len(b) == 2
+        return (a[0] - b[0], a[1] - b[1]) #simple way for debugging
 
     def _move_one(self, direction):
-        assert direction in self.direction_dict.keys()
-        new_head_loc = self.add_coordinates(self.head_loc, self.direction_dict[direction])
-        self.update_tail(new_head_loc, self.head_loc)
-        self.head_loc = new_head_loc
+        self._set_head_loc(self.add_coordinates(self.head_loc, self.direction_dict[direction]))
 
-    def update_tail(self, new_head_location, old_head_location):
-        col_dist, row_dist = self.get_distance(new_head_location, self.tail_loc)
-        if abs(row_dist) <= 1 and abs(col_dist) <= 1 or abs(col_dist) <= 1 and abs(row_dist) <= 1:
-            pass # range
-        elif (abs(row_dist) == 1 and abs(col_dist) == 1): # in diagonal range
-            pass
-        elif abs(row_dist) == 2 and col_dist == 0 or abs(col_dist) == 2 and row_dist == 0: # ortho move
-            self.tail_loc = old_head_location # direct follow
-        elif abs(row_dist) >= 1 and abs(col_dist) >= 1:# diag move, possible to see both values at 2 due to rope chaining
-            new_tail_row, new_tail_col = 0, 0
+    def _set_head_loc(self, new_location): # distances of new not checked, be careful
+        self.__update_tail(new_location) # may need to reference old location
+        self.head_loc = new_location 
+
+    def __update_tail(self, new_head_location):
+        row_dist, col_dist = self.sub_coordinates(new_head_location, self.tail_loc)
+        new_tail_row, new_tail_col = 0, 0
+        if abs(row_dist) == 2 and col_dist == 0: # ortho move, single update
+                new_tail_row = 1 if row_dist > 0 else -1
+        elif abs(col_dist) == 2 and row_dist == 0: # ortho move, single update
+                new_tail_col = 1 if col_dist > 0 else -1
+        elif abs(row_dist) + abs(col_dist) >= 3: # diag move, both require an update
             new_tail_row = 1 if row_dist > 0 else -1
             new_tail_col = 1 if col_dist > 0 else -1
-            self.tail_loc = self.add_coordinates(self.tail_loc, (new_tail_col, new_tail_row))
+        elif abs(row_dist) + abs(col_dist) <= 2: # skip if total magnitude is 2 or less (given that by now, one direction cannot be 2 magnitude)
+            pass
         else:
-            raise Exception("desync'd somehow, probably with _set_head_loc()")
+            raise Exception("error updating a tail, check if attempting to move tail more than 1 step")
+        new_tail_location = self.add_coordinates(self.tail_loc, (new_tail_row, new_tail_col))
+        self.tail_loc = new_tail_location
         self.tail_visited.add(self.tail_loc)
-    
-rope = Rope(10)
+
+segments = 9        
+rope = Rope(segments)
 for instr in instructions:
     rope.move(*instr.split())
     
-print(len(rope.tail_visited(0)))
-print(len(rope.tail_visited(9)))
-
-
+print(f"Rope with {2} knots tail occupancy: {len(rope.tail_visited(0))}")
+print(f"Rope with {10} knots tail occupancy: {len(rope.tail_visited(segments-1))}")
+# print(rope) # this is very pretty but the input given is too big to view nicely
