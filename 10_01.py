7db94cff4aff09093e00e1ae9d88a72995f13d43
nbovee <12849851+nbovee@users.noreply.github.com>
2022-12-17 18:44:31 -0500

    day 10 cleanup

--- 10_01.py
+++ 10_01.py
@@ -11,13 +11,10 @@ def signal_processing(_instructions, _responses):
         return True if x in range(base - dist, base + dist + 1) else False
 
     for instruction in _instructions:
-        cycle_step = 1
-        if 'noop' in instruction:
-            delta = 0
-        else:
+        cycle_step, delta = 1, 0
+        if 'addx' in instruction:
             delta = int(instruction.split()[-1])
             cycle_step += 1
-            
         for i in range(cycle_step):
             out+= '#' if in_draw_range() else ' '
             cycle += 1
@@ -28,5 +25,5 @@ def signal_processing(_instructions, _responses):
         print(out[i:i+40])
 
 intervals = [20, 60, 100, 140, 180, 220]
-l1 = list(signal_processing(instructions, intervals))
+l1 = signal_processing(instructions, intervals)
 print(f"Sum of interval responses for part 1: {sum(l1)}")
