34cc63047be3312d417d27f5591a7a18346ed513
nbovee <12849851+nbovee@users.noreply.github.com>
2023-12-05 19:08:54 -0500

    day 1

---
+++ 2023/01.py
@@ -0,0 +1,36 @@
+import common_advent as advent
+import re
+input = advent.get_input(__file__)
+
+conv = {
+    'one' : '1',
+    'two' : '2',
+    'three' : '3',
+    'four' : '4',
+    'five' : '5',
+    'six' : '6',
+    'seven' : '7',
+    'eight' : '8',
+    'nine' : '9',
+}
+
+val_re = re.compile("|".join(conv.values()))
+
+key_val_list = []
+for x in conv.items():
+    key_val_list.extend(x)
+key_val_re = re.compile("|".join(key_val_list))
+
+def day1(regex, replace = []):
+    sum = 0
+    for i in input:
+        rep = re.findall(regex, i)
+        if replace:
+            for e, r in enumerate(rep):
+                if r in replace:
+                    rep[e] = conv[r]
+        sum += int(f"{rep[0]}{rep[-1]}")
+    return sum
+
+print(day1(val_re))
+print(day1(key_val_re, replace=conv))
