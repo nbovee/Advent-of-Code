e8be371afd713de1efa4b3315222b5e2eee6fd99
nbovee <12849851+nbovee@users.noreply.github.com>
2023-12-17 18:37:34 -0500

    lazy d3.2

--- 2023/03.py
+++ 2023/03.py
@@ -10,17 +10,32 @@ def correct_bound(l_point, r_point):
     return (max(l_r, 0), max(l_c, 0)), (min(r_r, max_r), min(r_c, max_c))
 
 pn_list = []
-potential_list = set()
 pn_re = re.compile(r"\d+")
-symbol_re = re.compile(r"[^\d.]")
+symbol_re = re.compile(r"[^\d+.]")
+gear_re = re.compile(r"\*")
 for row, line in enumerate(input):
     possible = re.finditer(pn_re, line)
     for p in possible:
         p_int = int(p.group())
-        potential_list.add(p_int)
         s, e = correct_bound((row-1, p.span()[0]-1), (row+1, p.span()[1]+1))
         test_string = "".join([input[subrow][s[1]:e[1]] for subrow in range(s[0], e[0]+1)])
-        print(test_string)            
         if re.findall(symbol_re, test_string):
             pn_list.append(p_int)
 print(sum(pn_list))
+
+gear_score = 0
+for row, line in enumerate(input):
+    possible = re.finditer(gear_re, line)
+    for p in possible:
+        cogs = []
+        index = p.span()[0]
+        s, e = correct_bound((row-1, 0), (row+1, len(line)))
+        test_string = list([input[subrow] for subrow in range(s[0], e[0]+1)])
+        for r2 in test_string:
+            for p2 in re.finditer(pn_re, r2):
+                if not set(range(*p2.span())).isdisjoint(range(index - 1, index + 2)):
+                    cogs.append(p2)
+        if len(cogs)>=2:
+            gear_score += int(cogs[0].group()) * int(cogs[1].group())
+
+print(gear_score)
