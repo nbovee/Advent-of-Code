e1ac92ae78abbc31c32e734e1466baff8f2112ce
nbovee <12849851+nbovee@users.noreply.github.com>
2024-12-06 16:33:18 -0500

    finish 4, init 5

--- 2024/04/04.go
+++ 2024/04/04.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type xy struct {
@@ -22,9 +23,11 @@ func main() {
 
 	// Read each line of the file
 	xmas_found := 0
+	xmas_found_2 := 0
 	y := 0
 	m := make(map[xy]rune)
 	var starts = []xy{}
+	var starts_2 = []xy{}
 	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
 
@@ -36,6 +39,8 @@ func main() {
 			m[xy{x, y}] = char
 			if char == 'X' {
 				starts = append(starts, xy{x, y})
+			} else if char == 'A' {
+				starts_2 = append(starts_2, xy{x, y})
 			}
 
 		}
@@ -52,24 +57,34 @@ func main() {
 		xmas_found += checkOffset(1, -1, s, m)
 		xmas_found += checkOffset(-1, 1, s, m)
 		xmas_found += checkOffset(-1, -1, s, m)
-
+	}
+	for _, s := range starts_2 {
+		if checkOffset_2(s, m) {
+			xmas_found_2 += 1
+		}
 	}
 	fmt.Printf("P1 starts=%d, %d\n", len(starts), xmas_found)
-	fmt.Printf("P2 %q\n", 0)
+	fmt.Printf("P2 starts=%d, %d\n", len(starts_2), xmas_found_2)
 
 }
 
 func checkOffset(o_x int, o_y int, start xy, m map[xy]rune) int {
 	_mas := "MAS"
-	var disp string
-	disp = " X"
 	for i, char := range _mas {
 		this := xy{start.x + (i+1)*o_x, start.y + (i+1)*o_y}
 		if m[this] != char {
 			return 0
-		} else {
-			disp = disp + string(char)
 		}
 	}
 	return 1
+}
+
+func checkOffset_2(start xy, m map[xy]rune) bool {
+	// assemble a string from clockwise arbitrary start
+	var _check string
+	_check += string(m[xy{start.x + 1, start.y + 1}])
+	_check += string(m[xy{start.x - 1, start.y + 1}])
+	_check += string(m[xy{start.x - 1, start.y - 1}])
+	_check += string(m[xy{start.x + 1, start.y - 1}])
+	return strings.Contains("SSMMSSM", _check)
 }
